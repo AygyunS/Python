f624d01e9cd58cba682c2e1a6c0d71db01d203c9
AygyunS <32463645+AygyunS@users.noreply.github.com>
2023-01-21 14:40:10 +0000

    Push another Matrix training file

--- SoftUni/Advanced/4.MultidimensionalLists/Lab/1.Diagonals.py
+++ SoftUni/Advanced/4.MultidimensionalLists/Lab/1.Diagonals.py
@@ -1,22 +1,25 @@
 def read_matrix():
-    matrix = [[int(x) for x in input().split()]
-              for _ in range(int(input(", ")))]
+    matrix = [[int(x) for x in input().split(", ")]
+              for _ in range(int(input()))]
     return matrix
 
 def nums_diagonal(matrix):
     diagonal_nums = []
     for i in range(len(matrix)):
         diagonal_nums.append(matrix[i][i])
-    return diagonal_sum
+    return diagonal_nums
 
-def sum_of_diagonal_short(matrix):
-    diagonal_sum = [matrix[i][i] for i in range(len((matrix)))]
-    return diagonal_sum
-
+def num_sub_diagonal(matrix):
+    diagonal_nums = []
+    i=len(matrix)
+    for i in range(i, -1, -1):
+        diagonal_nums.append(matrix[i][i])
+    return diagonal_nums
 
 matrix = read_matrix()
 
 diagonal_nums = nums_diagonal(matrix)
+diagonal_sub_num = num_sub_diagonal(matrix)
 
-
-print(f"Primary diagonal: {(*sum_of_all)}. Sum: {sum(diagonal_nums)}"))
+print(f"Primary diagonal: {', '.join(map(str,diagonal_nums))}. Sum: {sum(diagonal_nums)}")
+print(f"Primary diagonal: {', '.join(map(str,diagonal_nums))}. Sum: {sum(diagonal_nums)}")
